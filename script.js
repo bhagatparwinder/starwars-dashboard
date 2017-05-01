2d6ec8a1e22ae3a3a2a28fb4bc271a273190af31
Parwinder Bhagat <singhparwinder@me.com>
2017-05-01 12:44:20 -0500

    Refactored template code
    Signed-off-by: Parwinder Bhagat <singhparwinder@me.com>

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
     Description: Contains code for Star Wars Dashboard
-    Version: 0.1
+    Version: 0.2
     Author: Parwinder Bhagat
  */
 
@@ -41,31 +41,29 @@ const graphOption = {
 //TODO: Remove backgroundColor or allow user to provide as input
 const graphData = {
     labels: [],
-    datasets: [
-        {
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(76, 159, 64, 1)'
-            ],
-            data: [],
-        }
-    ]
+    datasets: [{
+        backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(76, 159, 64, 1)'
+        ],
+        data: [],
+    }]
 };
 
 // Core functionality
 //==============================================
 
-var constructGraph = function(labels, scrollLength, graphData){
+var constructGraph = function (labels, scrollLength, graphData) {
     var currentGraphData = graphData;
 
     // Create local occurence of graphData. Do not modify global graphData
     currentGraphData.labels = labels,
-    currentGraphData.datasets[0].data = scrollLength;
+        currentGraphData.datasets[0].data = scrollLength;
 
     var ctx = document.getElementById("scroll-chart");
     new Chart(ctx, {
@@ -73,38 +71,43 @@ var constructGraph = function(labels, scrollLength, graphData){
         data: currentGraphData,
         options: graphOption
     });
-    
+
 };
 
-var createCardDeck = function(data){
-    var template =  '';
-    var mainArea = document.getElementById('card-holder');
-    for (let i = 0; i < data.length; i+=2){
-        var currentPoster = i+1,
-            nextPoster = i+2;
-        template += '<div class="card">' +
-        '<img class="card-img-top" src="assets/'+ currentPoster +'.jpg" alt="Poster">' +
-        '<img class="card-img-top" src="assets/'+ nextPoster +'.jpg" alt="Poster">' +
-        '<div class="card-block">' +
-        '<h4 class="card-title">'+ data[i].title +'</h4>' +
-        '<h4 class="card-title">'+ data[i].title +'</h4>' +
-        '<span class="card-text"><div>Director: '+ data[i].director +' </div>Cast: Han Solo, Wooki</span>' +
-        '<span class="card-text"><div>Director: '+ data[i].director +' </div>Cast: Han Solo, Wooki</span>' +
-        '</div>' +
+var constructTemplate = function (index, value) {
+    var render = '<div class="card-number-' + index + '">' +
+        '<img class="card-img-top" src="assets/' + index + '.jpg" alt="Poster">' +
+        '<span class="card-text"><h4 class="card-title">' + value.title + '</h4>' + 
+        '<span>Director: ' + value.director + ' </span>Cast: Han Solo, Wooki</span>' +
         '</div>';
+    return render;
+};
+
+var createCardDeck = function (data) {
+    var template = '';
+    var mainArea = document.getElementById('card-holder');
+    for (let i = 0; i <= data.length; i += 2) {
+        var current = i,
+            next = i + 1;
+        template += '<div class="card">';
+        template += constructTemplate(current, data[current]);
+        if (data[next]) {
+            template += constructTemplate(next, data[next]);
+        }
+        template += '</div>';
     }
     $(mainArea).append(template);
 
 };
 
-var prepData = function(data){
+var prepData = function (data) {
     var results = data,
         labels = [],
         scrollLength = [];
-    results.sort(function(a, b) {
+    results.sort(function (a, b) {
         return a.episode_id - b.episode_id;
     });
-    for (let i = 0; i< results.length; i++){
+    for (let i = 0; i < results.length; i++) {
         labels.push(results[i].title);
         scrollLength.push(results[i].opening_crawl.length);
     }
@@ -115,7 +118,7 @@ var prepData = function(data){
 // Trigger on document ready
 //==============================================
 
-$(document).ready(function(){
+$(document).ready(function () {
     $.ajax({
         type: "GET",
         url: "http://swapi.co/api/films/",
